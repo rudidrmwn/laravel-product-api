ffded5499f2554858f74b3762d357e15a07c111d
webart <rudi.darmawan@gmail.com>
2025-08-05 12:49:20 +0700

    route api

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,10 @@
 
 use App\Http\Controllers\ProductController;
 
-Route::apiResource('products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
